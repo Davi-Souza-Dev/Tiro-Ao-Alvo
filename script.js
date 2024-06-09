2c698760e682f4e377005f2be911d78ec4a7109b
Davi <149123949+Davi-Souza-Dev@users.noreply.github.com>
2024-06-09 20:13:54 -0300

    Bugs resolvidos

--- script.js
+++ script.js
@@ -5,34 +5,34 @@ const txtscore = document.getElementById("score");
 const btnReset = document.getElementById("btnReset");
 let larguraContainer = container.offsetWidth; //offsetWidth = pega o valor da largura atual da caixa
 let alturaContainer = container.offsetHeight; // offsetHeight = pega o valor da altura atual da caixa
-let musica = new Audio("musica.mp3");
-musica.play();
 let score = 0;
 let minutos = 0;
 let segundos = 59;
 let game = true;
 let som = new Audio("tiro.mp3");
 let fim = new Audio("win.mp3");
+let musica = new Audio("game.mp3");
+
 fim.currentTime = 0;
-let rodar = 500;
 //Cronometro
 const timer = setInterval((evt) => {
-    txtTime.innerHTML = `${minutos}:${segundos}`;
-    criarAlvos();
-    if (segundos > 0) {
-      segundos--;
-      segundos = segundos < 10 ? "0" + segundos : segundos;
-    }else if (segundos == 0 && minutos > 0) {
-      minutos--;
-      segundos = 59;
-      minutos = minutos < 10 ? "0" + minutos : minutos;
-    }else if(segundos == 0 && minutos == 0){
-        popup.style.display = "flex";
-        txtscore.innerHTML = `Score: ${score}`;
-        musica.pause();
-        fim.play();
-        clearInterval(timer);
-    }
+  musica.play();
+  txtTime.innerHTML = `${minutos}:${segundos}`;
+  criarAlvos();
+  if (segundos > 0) {
+    segundos--;
+    segundos = segundos < 10 ? "0" + segundos : segundos;
+  } else if (segundos == 0 && minutos > 0) {
+    minutos--;
+    segundos = 59;
+    minutos = minutos < 10 ? "0" + minutos : minutos;
+  } else if (segundos == 0 && minutos == 0) {
+    popup.style.display = "flex";
+    txtscore.innerHTML = `Score: ${score}`;
+    musica.pause();
+    fim.play();
+    clearInterval(timer);
+  }
 }, 500);
 
 //Caso a caixa seja redimensionada
@@ -73,22 +73,18 @@ class Alvo {
 }
 
 criarAlvos = () => {
-
   const alvo = new Alvo(container);
 
   const alvoContainer = document.getElementById(alvo.id);
   alvoContainer.addEventListener("click", (evt) => {
-
     som.play();
-
     score += 10;
     alvo.remove();
-  
   });
   som.pause();
   som.currentTime = 0;
 };
 
-btnReset.addEventListener("click",(evt)=>{
-    window.location.reload();
+btnReset.addEventListener("click", (evt) => {
+  window.location.reload();
 });
