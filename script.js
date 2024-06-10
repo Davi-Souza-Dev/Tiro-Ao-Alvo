f7dc890a5237229bbb43080cb240cefabd82e388
Davi <149123949+Davi-Souza-Dev@users.noreply.github.com>
2024-06-10 20:25:23 -0300

    Começo da estilização

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let musica = new Audio("game.mp3");
 musica.currentTime = 0;
 //Dificuldade e menu
@@ -17,10 +16,12 @@ let score = 0;
 let minutos = 0;
 let segundos = 59;
 let game = true;
+let bonus = 0;//Quantidade para saber se e para criar alvo bonus
 let som = new Audio("tiro.mp3");
 let fim = new Audio("win.mp3");
 let velo; //velocidade da criação de alvos
 fim.currentTime = 0;
+fim.volume = 0.1;
 let dificuldade;
 const playMusica = setInterval((evt) => {
   musica.play();
@@ -33,7 +34,6 @@ const playMusica = setInterval((evt) => {
 //Cronometro
 const iniciar = (velo) => {
   const timer = setInterval((evt) => {
-
     txtTime.innerHTML = `${minutos}:${segundos}`;
     criarAlvos();
     if (segundos > 0) {
@@ -50,7 +50,7 @@ const iniciar = (velo) => {
       clearInterval(timer);
     }
   }, velo);
-}
+};
 
 //Caso a caixa seja redimensionada
 window.addEventListener("resize", (evt) => {
@@ -62,7 +62,7 @@ window.addEventListener("resize", (evt) => {
 class Alvo {
   constructor(container) {
     //Definindo propriedades básicas
-    this.tam = Math.floor(Math.random() * 250) + 50;
+    this.tam = Math.floor(Math.random() * 150) + 50;
     this.X = Math.floor(Math.random() * (larguraContainer - this.tam));
     this.Y = Math.floor(Math.random() * (alturaContainer - this.tam));
     this.id = Date.now() + "_" + Math.floor(Math.random() * 10000);
@@ -71,9 +71,9 @@ class Alvo {
     this.desenhar();
     this.me = document.getElementById(`${this.id}`);
     //Remover após 1 segundos.
-    let sumir = setInterval(()=>{
+    let sumir = setInterval(() => {
       this.remove();
-    },1000)    
+    }, 1000);
   }
 
   desenhar = () => {
@@ -92,11 +92,11 @@ class Alvo {
   };
 }
 
-//Classe do alvo
-class AlvoErro {
+//Classe alvo bonus
+class AlvoBonus {
   constructor(container) {
     //Definindo propriedades básicas
-    this.tam = Math.floor(Math.random() * 250) + 50;
+    this.tam = Math.floor(Math.random() * 150) + 50;
     this.X = Math.floor(Math.random() * (larguraContainer - this.tam));
     this.Y = Math.floor(Math.random() * (alturaContainer - this.tam));
     this.id = Date.now() + "_" + Math.floor(Math.random() * 10000);
@@ -105,9 +105,42 @@ class AlvoErro {
     this.desenhar();
     this.me = document.getElementById(`${this.id}`);
     //Remover após 3 segundos.
-    let sumir = setInterval(()=>{
+    let sumir = setInterval(() => {
       this.remove();
-    },3000)    
+    }, 3000);
+  }
+
+  desenhar = () => {
+    const div = document.createElement("div");
+    div.setAttribute("class", "alvoBonus");
+    div.setAttribute("id", this.id);
+    div.setAttribute(
+      "style",
+      `top:${this.Y}px;left:${this.X}px;width:${this.tam}px;height:${this.tam}px`
+    );
+    this.box.appendChild(div);
+  };
+  remove = () => {
+    this.me.remove();
+  };
+}
+
+//Classe do alvoErro
+class AlvoErro {
+  constructor(container) {
+    //Definindo propriedades básicas
+    this.tam = Math.floor(Math.random() * 150) + 50;
+    this.X = Math.floor(Math.random() * (larguraContainer - this.tam));
+    this.Y = Math.floor(Math.random() * (alturaContainer - this.tam));
+    this.id = Date.now() + "_" + Math.floor(Math.random() * 10000);
+    this.box = container;
+    //Criando o objeto ao instanciar
+    this.desenhar();
+    this.me = document.getElementById(`${this.id}`);
+    //Remover após 3 segundos.
+    let sumir = setInterval(() => {
+      this.remove();
+    }, 1000);
   }
 
   desenhar = () => {
@@ -126,8 +159,9 @@ class AlvoErro {
   };
 }
 
+//Função para criar todos os alvos.
 criarAlvos = () => {
-  let escolha = Math.floor((Math.random() * dificuldade) + 1);
+  let escolha = Math.floor(Math.random() * dificuldade + 1);
   if (escolha < dificuldade) {
     const alvo = new Alvo(container);
     const alvoContainer = document.getElementById(alvo.id);
@@ -135,6 +169,7 @@ criarAlvos = () => {
       som.play();
       score += 10;
       alvo.remove();
+      bonus++;
     });
     som.pause();
     som.currentTime = 0;
@@ -145,21 +180,31 @@ criarAlvos = () => {
       som.play();
       alvoErro.remove();
       popup.style.display = "flex";
-      txtscore.innerHTML = "Você perdeu!!!!"
+      txtscore.innerHTML = "Você perdeu!!!!";
       clearInterval(timer);
     });
     som.pause();
     som.currentTime = 0;
   }
-}
-
+  if(bonus == 10){
+    bonus = 0;
+    const alvo = new AlvoBonus(container);
+    const alvoContainer = document.getElementById(alvo.id);
+    alvoContainer.addEventListener("click", (evt) => {
+      som.play();
+      score += 100;
+      alvo.remove();
+    });
+    som.pause();
+    som.currentTime = 0;
+  }
+};
 
 btnReset.addEventListener("click", (evt) => {
   window.location.reload();
 });
 
 //Configurar dificuldade do game
-
 btnFacil.addEventListener("click", (evt) => {
   minutos = 1;
   segundos = 59;
